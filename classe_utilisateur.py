dcbde97b1b9b0643b55d934b17f475d07439c8fd
ArthurGrimaldi <arthur.grimaldi.edu@groupe-gema.com>
2022-11-07 15:37:44 +0100

    Utilisateur : ajout Utilisateur_Admin

--- classe_utilisateur.py
+++ classe_utilisateur.py
@@ -2,16 +2,26 @@ import uuid
 from datetime import date, datetime
 
 class Utilisateur :
-    """
-    ### Purpose
-    Créer un Utilisateur commun (différent d'un Utilisateur_Admin).\n
-    Nécessite d'input un nom et une date de naissance (format date).\n
-    ID génère automatiquement des ID uniques en fonction de la datetime actuelle (import uuid)
-    """
-    
+
     def __init__(self, nom : str, date_naissance : date) -> None:
+        """
+        ### Purpose
+        Créer un Utilisateur commun (différent d'un Utilisateur_Admin).\n
+        Nécessite d'input un nom et une date de naissance (format date).\n
+        ID génère automatiquement des ID uniques en fonction de la datetime actuelle (import uuid)
+        
+        ### Args
+            nom (str): Nom de l'utilisateur
+            date_naissance (date): Date de naissance de l'utilisateur au format date
+        """
         self.id = uuid.uuid1()
         self.nom = nom
         self.date_naissance = date_naissance
         self.statut = "Utilisateur Commun"
         self.date_enregistrement = datetime.now()
+        
+class Utilisateur_Admin(Utilisateur):
+    
+    def __init__(self, nom: str, date_naissance: date) -> None:
+        super().__init__(nom, date_naissance)
+        self.statut = "Admin"
