ddb12f5b83bc2e891cc31348d88a49351b6592b5
PaulFaguet <paul.faguet.edu@groupe-gema.com>
2022-11-07 16:05:04 +0100

    modif in utilisateur

--- classes/classe_utilisateur.py
+++ classes/classe_utilisateur.py
@@ -1,27 +1,29 @@
 import uuid
 from datetime import date, datetime
 
-class Utilisateur :
 
-    def __init__(self, nom : str, date_naissance : date) -> None:
+class Utilisateur:
+
+    def __init__(self, nom: str, date_naissance: date) -> None:
         """
         ### Purpose
         Créer un Utilisateur commun (différent d'un Utilisateur_Admin).\n
         Nécessite d'input un nom et une date de naissance (format date).\n
         ID génère automatiquement des ID uniques en fonction de la datetime actuelle (import uuid)
-        
+
         ### Args
             nom (str): Nom de l'utilisateur
             date_naissance (date): Date de naissance de l'utilisateur au format date
         """
-        self.id = uuid.uuid1()
-        self.nom = nom
-        self.date_naissance = date_naissance
-        self.statut = "Utilisateur Commun"
-        self.date_enregistrement = datetime.now()
-        
+        self._id = uuid.uuid1()
+        self._nom = nom
+        self._date_naissance = date_naissance
+        self._statut = "Utilisateur Commun"
+        self._date_enregistrement = datetime.now()
+
+
 class Utilisateur_Admin(Utilisateur):
-    
+
     def __init__(self, nom: str, date_naissance: date) -> None:
         super().__init__(nom, date_naissance)
         self.statut = "Admin"
