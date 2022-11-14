f0e7e0dd7f3da5afd78df77fe4bacac4dc7f55fb
ArthurGrimaldi <arthur.grimaldi.edu@groupe-gema.com>
2022-11-14 16:45:53 +0100

    Ajout DocString emprunter() et retourner()

--- classes/utilisateur.py
+++ classes/utilisateur.py
@@ -50,6 +50,23 @@ class Utilisateur:
         return self._date_enregistrement
     
     def emprunter(self):
+        """
+        ### Purpose :
+        Méthode définie afin d'emprunter un livre.\n
+        Demande en input un titre et un auteur afin de ressortir l'ID du livre à emprunter.\n
+        Check :
+            - si le User a déjà fait un emprunt aujourd'hui 
+            - s'il a déjà 5 livres empruntés (Si oui ValueError).\n
+
+        ### Returns:
+            _type_: _description_
+            
+        ### Final :
+        Si toutes les conditions d'emprunt sont remplies, la méthode va :\n
+            - Ajouter l'ID du livre à la liste de livres empruntés par le User,
+            - Changer self.emprunt_jour à True pour User
+            - Changer self._statut à False pour Livre (False = emprunté)
+        """
         if self.emprunt == True:
             return "Vous ne pouvez plus emprunter de livres aujourd'hui. Revenez Demain !"
         if len(self._liste_livres) == 5 :
@@ -62,6 +79,28 @@ class Utilisateur:
     # Ajouter une variable contenant la date d'emprunt
     
     def retourner(self, livre = Livre):
+        """
+        ### Purpose :
+        Méthode définie afin de retourner un livre.\n
+        Demande en entrée un livre présent dans la base.\n
+        Check :
+            - Si le livre a bien été emprunté par le User,
+            - Si la note en input est un int,
+            - Si la note en input est entre 0 et 5.
+
+        Args:
+            livre (Livre, optional): Objet Livre. Defaults to Livre.
+
+        Returns:
+            _type_: _description_
+            
+        ### Final :
+        Si toutes les conditions de retour sont remplies, la méthode va :
+            - Ajouter la note à la liste de notes du livre,
+            - Retirer l'ID du livre de la liste d'emprunts de l'User,
+            - Changer self._statut du Livre à True,
+            - Ajouter à la liste _historique du livre l'ID de l'User, la date d'emprunt et la date de retour du livre.
+        """
         while True:
             try:
                 note = int(input("Entrer une note pour le livre :"))
@@ -74,7 +113,7 @@ class Utilisateur:
         livre._note.append(note)
         self._liste_livres = self._liste_livres.remove(livre._id)
         livre.statut = True
-        livre._historique = livre._historique.update({
+        livre._historique = livre._historique.append({
             "user": self._id,
             "date emprunt": self._liste_livres("date emprunt"),
             "date retour": datetime.now()
